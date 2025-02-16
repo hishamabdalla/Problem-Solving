7496d9ebf7d9ae19ccba977b423891e934716b3a
Hishvm <129236724+hishamabdalla@users.noreply.github.com>
2025-02-16 23:59:24 +0200

    [1] [Time Beats: 98.29%] [Memory Beats: 27.14%] - LeetPush

--- LeetCode/Easy/1-Two-Sum.cs
+++ LeetCode/Easy/1-Two-Sum.cs
@@ -1,15 +1,18 @@
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
+     public int[] TwoSum(int[] nums, int target)
+     {
+        var table=new Dictionary<int,int>();
         for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i + 1; j < nums.Length; j++)
+            int complated=target-nums[i];
+
+            if (table.ContainsKey(complated))
             {
-                if (nums[i] + nums[j] == target)
-                {
-                    return new int[] { i, j };
-                }
+                return new int[] { table[complated],i };
             }
-         }
+
+            table[nums[i]] = i;
+        }
         return new int[] { };
     }
 }
